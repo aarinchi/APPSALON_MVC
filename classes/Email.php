e2c838c46eefe3886d48b85828465bd4fb4fea0d
aarinchi <alescano970@fafi.utb.edu.ec>
2023-06-17 01:37:51 -0500

    first commit40

--- classes/Email.php
+++ classes/Email.php
@@ -79,7 +79,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Haz solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo. </p>";
-        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['SERVER_HOST'] . "/recuperar?token=" . $this->token . "'>Reestablecer Password </a> </p>";
+        $contenido .= "<p>Presiona aquí: <a href='" . "https://aaron-lescano-ejercicio.herokuapp.com" . "/recuperar?token=" . $this->token . "'>Reestablecer Password </a> </p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje </p>";
         $contenido .= "</html>";
 
